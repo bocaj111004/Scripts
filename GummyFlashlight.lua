393499125e3ff0a34c93b47faa1829d3ddb063d5
bocaj111004 <bocaj111004@gmail.com>
2025-03-07 21:01:56 +0000

    Update and rename Flashlight.lua to GummyFlashlight.lua

--- GummyFlashlight.lua
+++ GummyFlashlight.lua
@@ -1,20 +1,10 @@
- 
 local Modules = game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game.RemoteListener.Modules
 						local shears = game:GetObjects("rbxassetid://12794355024")[1]
 						shears.CanBeDropped = false
-						shears.Handle.Material = Enum.Material.Metal
-						shears.Handle.Color = Color3.fromRGB(75,79,94)
-						shears.Handle.Transparency = 0 
-						shears.Handle.Dark.Color = Color3.fromRGB(91,93,105)
-						shears.Handle.Dark.Material = Enum.Material.Metal
-						shears.Handle.Glass.Color = Color3.fromRGB(255,255,255)
-						shears.Handle.Switch.Color = Color3.fromRGB(145,146,162)
 						shears.Parent = game.Players.LocalPlayer.Backpack
-						
-						shears:WaitForChild("Handle").SpotLight.Brightness = 3
+						shears:WaitForChild("Handle").SpotLight.Brightness = 2
 						shears:WaitForChild("Handle").SpotLight.Range = 75
-						shears:WaitForChild("Handle").SpotLight.Angle = 60
-						shears:WaitForChild("Handle").SpotLight.Color = Color3.fromRGB(189, 149, 113)
+						shears:WaitForChild("Handle").SpotLight.Angle = 50
 						shears:SetAttribute("LightSourceBeam",true)
 						shears:SetAttribute("LightSourceStrong",true)
 						shears:SetAttribute("Enabled",false)
@@ -23,16 +13,16 @@ local Modules = game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game.Re
 
 						shears:SetAttribute("NamePlural","Shakelights")
 						shears:SetAttribute("NameSingular","Shakelight")
-						local newCFrame = CFrame.new(-0.094802849, -0.00991820451, 0.0960054174, 0, 0, -1, -1, 0, 0, 0, 1, 0)
+						local newCFrame = CFrame.new(0.00991821289, -0.17137143, 0.0771455616, 1, 0, 0, 0, 0, -1, 0, 1, 0)
 
 						shears.Grip = newCFrame
 						shears.WorldPivot = CFrame.new(249.886551, 1.53111672, -16.8949146, -0.765167952, 0.00742102973, 0.64378804, -0.000446901657, 0.999927223, -0.0120574543, -0.643830597, -0.00951368641, -0.765108943)
-						shears.Name = "Flashlight"
+						shears.Name = "Shakelight"
 						local Animations_Folder = Instance.new("Folder")
 						Animations_Folder.Name = "Animations"
 						Animations_Folder.Parent = shears
 						local Shake_Animation = Instance.new("Animation")
-						Shake_Animation.AnimationId = "rbxassetid://15386224888"
+						Shake_Animation.AnimationId = "rbxassetid://12001275923"
 						Shake_Animation.Parent = Animations_Folder
 						local Idle_Animation = Instance.new("Animation")
 						Idle_Animation.AnimationId = "rbxassetid://11372556429"
@@ -43,11 +33,12 @@ local Modules = game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game.Re
 						local Shake_Sound = Instance.new("Sound")
 						Shake_Sound.Name = "Shake_Sound"
 						Shake_Sound.Parent = shears
-						Shake_Sound.Volume = 1
-						Shake_Sound.SoundId = "rbxassetid://9114481260"
+						Shake_Sound.SoundId = "rbxassetid://11374330092"
 						Shake_Sound.PlaybackSpeed = 0.9
+						Shake_Sound.Volume = 1
 						local Shaking = false
-						shears.TextureId = "rbxassetid://16680616231"
+
+						shears.TextureId = "rbxassetid://11373085609"
 
 						local Animator = game.Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):FindFirstChildOfClass("Animator")
 						local anim2 = Animator:LoadAnimation(Idle_Animation)
@@ -58,7 +49,7 @@ local Modules = game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game.Re
 						anim3:Play()
 
 						local anim = Animator:LoadAnimation(Shake_Animation)	
-local enabled = false
+
 						shears.Activated:Connect(function()
 							if Shaking == false and shears.Parent == game.Players.LocalPlayer.Character then
 
@@ -74,19 +65,6 @@ local enabled = false
 								anim:Play()
 
 								Shake_Sound:Play()
-								enabled = not enabled
-								if enabled == true then
-									if Modules:FindFirstChild("Screech") then
-										Modules.Screech.Name = "Screech_"
-									end	
-								else
-									
-									if Modules:FindFirstChild("Screech_") then
-										if AntiScreech == false then
-											Modules["Screech_"].Name = "Screech"
-										end
-									end
-								end
 
 								task.wait(0.25)
 
@@ -96,29 +74,22 @@ local enabled = false
 							end
 
 						end)
-						game:GetService("RunService").RenderStepped:Connect(function()
-							if enabled == true then
-								shears:WaitForChild("Handle").SpotLight.Enabled = true
-								Shake_Sound.PlaybackSpeed = 1
-								
-							else
-								shears:WaitForChild("Handle").SpotLight.Enabled = false
-								Shake_Sound.PlaybackSpeed = 0.7
-								
-							end
-						end)
 						shears.Equipped:Connect(function()
 							anim3:Play()
 							anim2:Play()
-							
+							if Modules:FindFirstChild("Screech") then
+								Modules.Screech.Name = "Screech_"
+							end
 						end)
 						shears.Unequipped:Connect(function()
 							anim2:Stop()
-							enabled = false
 							if Modules:FindFirstChild("Screech_") then
-								if AntiScreech == false then
+								
 									Modules["Screech_"].Name = "Screech"
-								end
+								
 							end
-							
-						end)	
+						end)
+
+
+
+					end	
