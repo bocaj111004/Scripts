937cde4b18789f2a89db95e4257f3643026052d3
bocaj111004 <bocaj111004@gmail.com>
2025-05-28 20:04:56 +0100

    Create DoorsSpeedBypass.lua

---
+++ DoorsSpeedBypass.lua
@@ -0,0 +1,26 @@
+local Player = game:GetService('Players').LocalPlayer
+local Character = Player.Character
+
+local Collision = Character:WaitForChild("Collision", 9e9)
+local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart", 9e9)
+local CollisionClone = Collision:Clone()
+CollisionClone.Name = "ClonedCollision"
+CollisionClone.Parent = Character
+CollisionClone.CanCollide = false
+CollisionClone.CanTouch = false
+CollisionClone.CanQuery = false
+CollisionClone.Massless = true
+
+getgenv().SpeedBypass = false
+
+while true do
+if getgenv().SpeedBypass == true then
+	if HumanoidRootPart.Anchored then
+		CollisionClone.Massless = true
+	else
+		CollisionClone.Massless = not CollisionClone.Massless
+	end
+	
+	task.wait(0.23)
+end
+end
