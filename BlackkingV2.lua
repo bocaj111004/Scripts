e5bcf1e331e97fc58066ba08fd94d19169787a61
bocaj111004 <bocaj111004@gmail.com>
2025-07-09 16:22:18 +0100

    Update BlackkingV2.lua

--- BlackkingV2.lua
+++ BlackkingV2.lua
@@ -2,7 +2,7 @@ local Image = "http://www.roblox.com/asset/?id=6558374856"
 --local Image = "http://www.roblox.com/asset/?id=8508980527"
 
 for i,Part in pairs(workspace:GetDescendants()) do
-	if Part:IsA("BasePart") and Part.Transparency == 0 and not game.Players:GetPlayerFromCharacter(Part.Parent) and not game.Players:GetPlayerFromCharacter(Part.Parent.Parent) then
+	if Part:IsA("BasePart") and not game.Players:GetPlayerFromCharacter(Part.Parent) and not game.Players:GetPlayerFromCharacter(Part.Parent.Parent) then
 local Decals = {}
 		local Decal = Instance.new("Decal")
 		Decal.Face = Enum.NormalId.Front
@@ -47,32 +47,38 @@ local Decals = {}
 end
 
 workspace.DescendantAdded:Connect(function(Part)
-	if Part:IsA("BasePart") and Part.Transparency == 0 and not game.Players:GetPlayerFromCharacter(Part.Parent) and not game.Players:GetPlayerFromCharacter(Part.Parent.Parent)  then
+	if Part:IsA("BasePart") and not game.Players:GetPlayerFromCharacter(Part.Parent) and not game.Players:GetPlayerFromCharacter(Part.Parent.Parent)  then
 local Decals = {}
 		local Decal = Instance.new("Decal")
 		Decal.Face = Enum.NormalId.Front
 		Decal.Parent = Part
 		Decal.Texture = Image
+		table.insert(Decals, Decal)
 		local Decal = Instance.new("Decal")
 		Decal.Face = Enum.NormalId.Back
 		Decal.Parent = Part
 		Decal.Texture = Image
+		table.insert(Decals, Decal)
 		local Decal = Instance.new("Decal")
 		Decal.Face = Enum.NormalId.Top
 		Decal.Parent = Part
 		Decal.Texture = Image
+		table.insert(Decals, Decal)
 		local Decal = Instance.new("Decal")
 		Decal.Face = Enum.NormalId.Bottom
 		Decal.Parent = Part
 		Decal.Texture = Image
+		table.insert(Decals, Decal)
 		local Decal = Instance.new("Decal")
 		Decal.Face = Enum.NormalId.Left
 		Decal.Parent = Part
 		Decal.Texture = Image
+		table.insert(Decals, Decal)
 		local Decal = Instance.new("Decal")
 		Decal.Face = Enum.NormalId.Right
 		Decal.Parent = Part
 		Decal.Texture = Image
+		table.insert(Decals, Decal)
 		Part:GetPropertyChangedSignal("Transparency"):Connect(function()
 		
 				for i,Decal in pairs(Decals) do
